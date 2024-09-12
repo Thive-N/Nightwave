a8ed58f57ea3b7950d4d09ae36d5fe4d81a31576
Thive-N <35178093+Thive-N@users.noreply.github.com>
2024-09-12 16:02:53 +0100

    More rss parsing capabilities and comments

--- lib/rss.ts
+++ lib/rss.ts
@@ -26,7 +26,30 @@ export const extractImageURL = async (url: string) => {
   }
   return urls[0].url;
 };
+
+
+/**
+ * Returns a single feed from a rss feed url
+ *
+ * @param url - the url to fetch the data from
+ * @returns the feed
+ */
 export const fetchFeed = async (url: string) => {
   const parser = new Parser();
   return await parser.parseURL(url);
 };
+
+/**
+ * Returns a condensed list of all feeds in the urls array
+ *
+ * @param urls - a list of the urls to fetch the data from
+ * @returns a condensed list of the feeds identical
+ */
+export fetchMultipleFeeds = async (urls:string[]) => {
+  const parser = new Parser();
+  let ret = [];
+  urls.forEach(element => {
+    ret.concat(await parser.parse(element));
+  });
+  return ret;
+}
